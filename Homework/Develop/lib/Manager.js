6578d1e6db0b35b1ef8d412d86b790d53b830727
Raymond Amparo <ray.amparo1995@gmail.com>
2020-02-20 18:53:40 -0800

    Full Homework

---
+++ Homework/Develop/lib/Manager.js
@@ -0,0 +1,13 @@
+const Employee = require('./Employee');
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber){
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+        this.role = 'Manager'
+        this.getOfficeNumber = function(){
+            return this.officeNumber;
+        }
+    }
+}
+module.exports = Manager;
